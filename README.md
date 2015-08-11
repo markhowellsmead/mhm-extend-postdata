7c74b09c5d87e977526c55a810c8a274770f398c
Mark Howells-Mead <m@mhm.li>
2015-08-11 20:23:25 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# mhm-extend-postdata
+# Extend post data
 Automatically extends Post and Page data with an array containing all of the extra meta data pertinent to the $post object.
+
+## Usage
+Install the plugin. The plugin hooks into `the_post()` and adds all post meta data to `$post->meta_data`.
+
+##Author
+Mark Howells-Mead | www.permanenttourist.ch/github | Since August 2015
+
+##License
+Use this code freely, widely and for free. Provision of this code provides and implies no guarantee.
+
+Please respect the GPL v2 licence, which is available via http://www.gnu.org/licenses/gpl-2.0.html
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Extend Post Data
+Plugin URI: https://github.com/mhmli/mhm-extend-postdata
+Description: Automatically extends Post and Page data with an array containing all of the extra meta data pertinent to the $post object.
+Author: Mark Howells-Mead
+Version: 1.0
+Author URI: http://permanenttourist.ch/
+*/
+
+class MHM_ExtendPostdata {
+	
+	var $key = '';
+	
+	function dump($var,$die=false){
+		echo '<pre>' .print_r($var,1). '</pre>';
+		if($die){die();}
+	}//dump
+
+	//////////////////////////////////////////////////
+
+	public function __construct(){
+		$this->key = basename(__DIR__);
+		add_action( 'the_post', array(&$this, 'extend_postdata') );
+	}
+
+	//////////////////////////////////////////////////
+	
+	public function extend_postdata($post) {
+		$post->metadata = get_post_meta ( $post->ID );
+	}
+
+}
+
+new MHM_ExtendPostdata();
